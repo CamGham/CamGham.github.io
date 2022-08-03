18293f2ff4c7fe35a63afb8e82f19c89d469f8fe
CamGham <greayham@gmail.com>
2022-08-03 17:49:36 +1200

    Updates

---
+++ precache-manifest.6a59ac0bd1cbf3dddf340763fe84f2f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "204c58346d4b161c646f",
+    "url": "/static/css/main.f484752d.chunk.css"
+  },
+  {
+    "revision": "204c58346d4b161c646f",
+    "url": "/static/js/main.e37967ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4a84e0341505617a3ae",
+    "url": "/static/js/2.a7ae1457.chunk.js"
+  },
+  {
+    "revision": "e2ca545a7ce3e533fddd09f1523f5c0d",
+    "url": "/static/media/SportsHub.e2ca545a.png"
+  },
+  {
+    "revision": "9f23c02ac31d661d360ebbc1d85b2684",
+    "url": "/static/media/CabsOnline.9f23c02a.png"
+  },
+  {
+    "revision": "24515d181f498b81ede1b0c53eafe938",
+    "url": "/static/media/3- CV Template 2022 v3.24515d18.pdf"
+  },
+  {
+    "revision": "07733e507502972a158e9da15bca07ec",
+    "url": "/index.html"
+  }
+];
